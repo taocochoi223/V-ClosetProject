bcdc309dbae7412c245cdf03c5f156c2c5681fe3
S-bon-debug <sonngocson25@gmail.com>
2026-06-14 09:37:59 +0700

    feat: implement chat controller and service for room management and messaging functionality

--- VCloset.Application/Interfaces/IChatService.cs
+++ VCloset.Application/Interfaces/IChatService.cs
@@ -13,6 +13,8 @@ public interface IChatService
     Task<ChatRoomResponseDto> CreateDirectRoomAsync(int userId, CreateDirectRoomRequest request);
     Task<ChatRoomResponseDto> CreateGroupRoomAsync(int userId, CreateGroupRoomRequest request);
     Task<bool> AddMembersToGroupAsync(int userId, Guid roomId, AddGroupMembersRequest request);
+    Task<List<ChatRoomMemberResponseDto>> GetRoomMembersAsync(int userId, Guid roomId);
+    Task<bool> RemoveMemberFromGroupAsync(int userId, Guid roomId, Guid targetUserId);
     Task<List<ChatRoomResponseDto>> GetChatRoomsAsync(int userId);
     Task<bool> LeaveGroupRoomAsync(int userId, Guid roomId);
     Task<bool> MarkMessagesAsReadAsync(int userId, Guid roomId);
