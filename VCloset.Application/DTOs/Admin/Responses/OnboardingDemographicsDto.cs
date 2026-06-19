4a25380282fc83f0d55804eff8be4b1d30eef196
hoangqui794 <truongqui858@gmail.com>
2026-06-19 22:18:38 +0700

    fix(admin): remove BodyShapes from onboarding demographics, add Country demographics tracking

--- VCloset.Application/DTOs/Admin/Responses/OnboardingDemographicsDto.cs
+++ VCloset.Application/DTOs/Admin/Responses/OnboardingDemographicsDto.cs
@@ -5,7 +5,6 @@ namespace VCloset.Application.DTOs.Admin.Responses;
 public class OnboardingDemographicsDto
 {
     public int TotalCompletedOnboarding { get; set; }
-    public Dictionary<string, int> BodyShapes { get; set; } = new Dictionary<string, int>();
     public Dictionary<string, int> Lifestyles { get; set; } = new Dictionary<string, int>();
     public Dictionary<string, int> EyeColors { get; set; } = new Dictionary<string, int>();
     public Dictionary<string, int> HairColors { get; set; } = new Dictionary<string, int>();
@@ -13,4 +12,5 @@ public class OnboardingDemographicsDto
     // Optional demographics
     public Dictionary<string, int> Genders { get; set; } = new Dictionary<string, int>();
     public Dictionary<string, int> AgeGroups { get; set; } = new Dictionary<string, int>();
+    public Dictionary<string, int> Countries { get; set; } = new Dictionary<string, int>();
 }
